e711a20eea1f19a7e9ea7ae18a126292754d8b44
Bruno Duarte <bruno.duarte314@gmail.com>
2025-01-07 13:09:06 -0300

    Add notification interceptor when application is open

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,48 @@
-import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { useTheme } from "native-base";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 
-import { AppRoutes } from './app.routes';
+import { AppRoutes } from "./app.routes";
+import { useEffect, useState } from "react";
+import {
+  NotificationWillDisplayEvent,
+  OneSignal,
+  OSNotification,
+} from "react-native-onesignal";
+import { Notification } from "../components/Notification";
 
 export function Routes() {
+  const [notification, setNotification] = useState<OSNotification>();
   const { colors } = useTheme();
-
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  const onCloseNotification = () => setNotification(undefined);
+
+  useEffect(() => {
+    const handleGetNotification = (event: NotificationWillDisplayEvent) => {
+      event.preventDefault();
+      setNotification(event.getNotification());
+    };
+
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay",
+      handleGetNotification
+    );
+
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        "foregroundWillDisplay",
+        handleGetNotification
+      );
+  }, []);
 
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {!!notification?.title && (
+        <Notification title={notification.title} onClose={onCloseNotification} />
+      )}
     </NavigationContainer>
   );
 }
